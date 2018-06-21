891c96736d8cb4e6b26ffc88bf95d8a6da741c3f
jocelio <jocelio27@gmail.com>
2018-06-20 23:40:46 -0300

    importing lodash

--- src/js/components/new-quiz/Answer.js
+++ src/js/components/new-quiz/Answer.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { connect } from 'react-redux';
+import React, { Component } from "react"
+import { connect } from 'react-redux'
+import _ from 'lodash'
 import { setAnswerStatement } from '../../actions/quizActions'
-
 
 class Answer extends Component {
 
